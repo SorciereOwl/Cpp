de499d9204627b5927a434ffba16aae80d97685b
Sorciere <yefanova04@inbox.ru>
2023-09-18 17:29:03 +0300

    Revert "Cpp"
    This reverts commit 54133def740d1b899329032d809333d4812a397d.

--- MatrixTransposition(Summ,Mult)/Matrix.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-int** Init(int& n, int& m);
-void Trans(int** Matr, int** A, int n, int m);
-void PrintMatrix(int** Matr, int n, int m);
-int** Summ(int** A, int** T, int n, int m);
-int** Mult(int** A, int** T, int n1, int m1, int n2, int m2);
-
-int** UnTrans(int m, int n);
-void DeleteMatr(int** A, int n);
